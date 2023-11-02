24c3cccf74f0b1b68069edd39ddbe5728dba9592
Nadya Solomennikova <salamenikava77@gmail.com>
2023-11-02 11:56:36 +0300

    add proj

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import '../style/App.scss'
+import pic1 from '../assets/pic1.jpg'
+function App() {
+
+  return (
+    <>
+    <h1>Nadya</h1>
+    <img src={pic1} alt="" />
+    <div className="box1"></div>
+    </>
+  )
+}
+
+export default App
